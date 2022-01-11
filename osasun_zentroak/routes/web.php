abe16c8adccf9c7a096322afcdb1767c64dcd19d
asiergoieen <60966539+asiergoien@users.noreply.github.com>
2022-01-11 10:27:40 +0100

    ejemplo - Para vistas

--- osasun_zentroak/routes/web.php
+++ osasun_zentroak/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index',[IndexController::class,'cargarvista']);
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
