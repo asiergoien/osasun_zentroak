c812d7d9bdbd6286f79c1ce492e61486a830aea7
MarkelKN <90388221+MarkelKN@users.noreply.github.com>
2022-01-20 13:28:18 +0100

    cambios minusculos, apenas hay cambios, pero hago el commit por si acaso :)

--- osasun_zentroak/resources/views/layouts/nav.blade.php
+++ osasun_zentroak/resources/views/layouts/nav.blade.php
@@ -13,7 +13,16 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active text-light" aria-current="page" href="/osasun-zentroak">Osasun zentroak</a>
+                    <a class="nav-link active text-light" aria-current="page" href="/osasun-zentroak">Guztiak</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active text-light" aria-current="page" href="/osasun-zentroak?probintzia=Bizkaia">Bizkaia</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active text-light" aria-current="page" href="/osasun-zentroak?probintzia=Gipuzkoa">Gipuzkoa</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active text-light" aria-current="page" href="/osasun-zentroak?probintzia=Araba">Araba</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
