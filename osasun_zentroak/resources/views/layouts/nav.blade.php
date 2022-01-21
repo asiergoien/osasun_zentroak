6d8accf4589422079ef1950a7e4742b510629ec2
MarkelKN <90388221+MarkelKN@users.noreply.github.com>
2022-01-21 12:27:38 +0100

    perdown, habia cosas que no iban :pray:

--- osasun_zentroak/resources/views/layouts/nav.blade.php
+++ osasun_zentroak/resources/views/layouts/nav.blade.php
@@ -89,32 +89,3 @@
 <main id="app">
     @yield('content')
 </main>
-
-<!-- Back to Top -->
-<a href="#" class="back-to-top"></a>
-<!-- Script -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-<script>
-    $(document).ready(function() {
-
-        //Check to see if the window is top if not then display button
-        $(window).scroll(function() {
-            // Show button after 100px
-            var showAfter = 100;
-            if ($(this).scrollTop() > showAfter) {
-                $('.back-to-top').fadeIn();
-            } else {
-                $('.back-to-top').fadeOut();
-            }
-        });
-
-        //Click event to scroll to top
-        $('.back-to-top').click(function() {
-            $('html, body').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
-        });
-
-    });
-</script>
